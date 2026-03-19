a3b0a9adc758b44bf2f0c47b3163c17c5fc270a1
psbsj2016 <psbsj.2016@gmail.com>
2026-03-19 13:42:40 -0300

    Ajuste elastico do layout do carne e versao 12 do cache

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // SERVICE WORKER - MOTOR DO PWA (APP INSTALÁVEL)
 // =========================================================
 
-const CACHE_NAME = 'escola-pwa-v12';
+const CACHE_NAME = 'escola-pwa-v13';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
