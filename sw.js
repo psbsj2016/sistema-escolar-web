8a7ab7b7ae6b2c09643a0540dd85f7d5c7e825ee
psbsj2016 <psbsj.2016@gmail.com>
2026-03-19 16:02:11 -0300

    Melhoria Global de UX: Cursor pointer em todos os elementos interativos e botoes

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // SERVICE WORKER - MOTOR DO PWA (APP INSTALÁVEL)
 // =========================================================
 
-const CACHE_NAME = 'escola-pwa-v16';
+const CACHE_NAME = 'escola-pwa-v17';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
