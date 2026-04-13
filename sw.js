cd1341a487590f2129b60c04d18b7e4d53be0629
psbsj2016 <psbsj.2016@gmail.com>
2026-04-12 22:23:15 -0300

    Auto-Ajuste com faltas e sincronização total de cache

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // 1. GESTÃO DE VERSÃO DINÂMICA
 const APP_PREFIX = 'escola-pwa-';
 // 🚀 SEMPRE QUE ATUALIZAR O SISTEMA FRONT-END, MUDE ESTE NÚMERO
-const VERSION = 'v2.0.7'; 
+const VERSION = 'v2.0.8'; 
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 // Ficheiros essenciais para o ecrã inicial funcionar super rápido
@@ -13,12 +13,12 @@ const ASSETS_TO_CACHE = [
   '/',
     '/index.html',
     '/css/site.css',
-    '/js/config.js',
-    '/js/app.js',
-    '/js/modulos/cadastros.js',
-    '/js/modulos/financeiro.js',
-    '/js/modulos/pedagogico.js',
-    '/js/modulos/relatorios.js'
+    '/js/config.js?v=5',
+    '/js/app.js?v=5',
+    '/js/modulos/cadastros.js?v=5',
+    '/js/modulos/financeiro.js?v=5',
+    '/js/modulos/pedagogico.js?v=5',
+    '/js/modulos/relatorios.js?v=5'
 ];
 
 // 2. INSTALAÇÃO: Força a ativação imediata e guarda os ficheiros estáticos
