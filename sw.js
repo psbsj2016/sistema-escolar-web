a43d186e1b86734b327720f548f50987c0a127ad
psbsj2016 <psbsj.2016@gmail.com>
2026-04-19 15:54:26 -0300

    Adiciona mascara de CPF na tela de matricula

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // 1. GESTÃO DE VERSÃO DINÂMICA
 const APP_PREFIX = 'escola-pwa-';
 // 🚀 SEMPRE QUE ATUALIZAR O SISTEMA FRONT-END, MUDE ESTE NÚMERO
-const VERSION = 'v5.1.5'; 
+const VERSION = 'v5.1.6'; 
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 // Ficheiros essenciais para o ecrã inicial funcionar super rápido
@@ -13,12 +13,12 @@ const ASSETS_TO_CACHE = [
   '/',
     '/index.html',
     '/css/site.css',
-    '/js/config.js?v=24',
-    '/js/app.js?v=24',
-    '/js/modulos/cadastros.js?v=24',
-    '/js/modulos/financeiro.js?v=24',
-    '/js/modulos/pedagogico.js?v=24',
-    '/js/modulos/relatorios.js?v=24'
+    '/js/config.js?v=25',
+    '/js/app.js?v=25',
+    '/js/modulos/cadastros.js?v=25',
+    '/js/modulos/financeiro.js?v=25',
+    '/js/modulos/pedagogico.js?v=25',
+    '/js/modulos/relatorios.js?v=25'
 ];
 
 // 2. INSTALAÇÃO: Força a ativação imediata e guarda os ficheiros estáticos
