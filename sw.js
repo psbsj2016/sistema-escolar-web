d61133f68509b43e240e92dd70c3b3bff4e9f73c
psbsj2016 <psbsj.2016@gmail.com>
2026-05-01 17:05:13 -0300

    Atualização do sistema e página de matrícula

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // 1. GESTÃO DE VERSÃO DINÂMICA
 const APP_PREFIX = 'escola-pwa-';
 // 🚀 SEMPRE QUE ATUALIZAR O SISTEMA FRONT-END, MUDE ESTE NÚMERO
-const VERSION = 'v1777665688'; 
+const VERSION = 'v1777665907'; 
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 // Ficheiros essenciais para o ecrã inicial funcionar super rápido
@@ -13,12 +13,12 @@ const ASSETS_TO_CACHE = [
   '/',
     '/index.html',
     '/css/site.css',
-    '/js/config.js?v=1777665688',
-    '/js/app.js?v=1777665688',
-    '/js/modulos/cadastros.js?v=1777665688',
-    '/js/modulos/financeiro.js?v=1777665688',
-    '/js/modulos/pedagogico.js?v=1777665688',
-    '/js/modulos/relatorios.js?v=1777665688'
+    '/js/config.js?v=1777665907',
+    '/js/app.js?v=1777665907',
+    '/js/modulos/cadastros.js?v=1777665907',
+    '/js/modulos/financeiro.js?v=1777665907',
+    '/js/modulos/pedagogico.js?v=1777665907',
+    '/js/modulos/relatorios.js?v=1777665907'
 ];
 
 // 2. INSTALAÇÃO: Força a ativação imediata e guarda os ficheiros estáticos
