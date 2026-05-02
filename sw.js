471d5e536fc3c518cb04be4be88adbc35bfe273f
psbsj2016 <psbsj.2016@gmail.com>
2026-05-02 20:15:39 -0300

    UI Force: Blindagem de CSS com !important para responsividade mobile

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // 1. GESTÃO DE VERSÃO DINÂMICA
 const APP_PREFIX = 'escola-pwa-';
 // 🚀 SEMPRE QUE ATUALIZAR O SISTEMA FRONT-END, MUDE ESTE NÚMERO
-const VERSION = 'v1777763352'; 
+const VERSION = 'v1777763736'; 
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 // Ficheiros essenciais para o ecrã inicial funcionar super rápido
@@ -13,12 +13,12 @@ const ASSETS_TO_CACHE = [
   '/',
     '/index.html',
     '/css/site.css',
-    '/js/config.js?v=1777763352',
-    '/js/app.js?v=1777763352',
-    '/js/modulos/cadastros.js?v=1777763352',
-    '/js/modulos/financeiro.js?v=1777763352',
-    '/js/modulos/pedagogico.js?v=1777763352',
-    '/js/modulos/relatorios.js?v=1777763352'
+    '/js/config.js?v=1777763736',
+    '/js/app.js?v=1777763736',
+    '/js/modulos/cadastros.js?v=1777763736',
+    '/js/modulos/financeiro.js?v=1777763736',
+    '/js/modulos/pedagogico.js?v=1777763736',
+    '/js/modulos/relatorios.js?v=1777763736'
 ];
 
 // 2. INSTALAÇÃO: Força a ativação imediata e guarda os ficheiros estáticos
