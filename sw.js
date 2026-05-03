08d805b90add398ca1b37597892fd287e68ce2c9
psbsj2016 <psbsj.2016@gmail.com>
2026-05-03 19:15:09 -0300

    Ajuste contratos

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // 1. GESTÃO DE VERSÃO DINÂMICA
 const APP_PREFIX = 'escola-pwa-';
 // 🚀 SEMPRE QUE ATUALIZAR O SISTEMA FRONT-END, MUDE ESTE NÚMERO
-const VERSION = 'v1777846053'; 
+const VERSION = 'v1777846504'; 
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 // Ficheiros essenciais para o ecrã inicial funcionar super rápido
@@ -13,12 +13,12 @@ const ASSETS_TO_CACHE = [
   '/',
     '/index.html',
     '/css/site.css',
-    '/js/config.js?v=1777846053',
-    '/js/app.js?v=1777846053',
-    '/js/modulos/cadastros.js?v=1777846053',
-    '/js/modulos/financeiro.js?v=1777846053',
-    '/js/modulos/pedagogico.js?v=1777846053',
-    '/js/modulos/relatorios.js?v=1777846053'
+    '/js/config.js?v=1777846504',
+    '/js/app.js?v=1777846504',
+    '/js/modulos/cadastros.js?v=1777846504',
+    '/js/modulos/financeiro.js?v=1777846504',
+    '/js/modulos/pedagogico.js?v=1777846504',
+    '/js/modulos/relatorios.js?v=1777846504'
 ];
 
 // 2. INSTALAÇÃO: Força a ativação imediata e guarda os ficheiros estáticos
