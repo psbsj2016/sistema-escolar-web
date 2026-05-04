3bfdcb0f54d33b69233ccd912eea6a7717b2d1c9
psbsj2016 <psbsj.2016@gmail.com>
2026-05-04 18:57:38 -0300

    🚀 Correção CDN e Fura-Cache adicionado

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // 1. GESTÃO DE VERSÃO DINÂMICA
 const APP_PREFIX = 'escola-pwa-';
 // 🚀 SEMPRE QUE ATUALIZAR O SISTEMA FRONT-END, MUDE ESTE NÚMERO
-const VERSION = 'v1777929942'; 
+const VERSION = 'v1777931853'; 
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 // Ficheiros essenciais para o ecrã inicial funcionar super rápido
@@ -13,12 +13,12 @@ const ASSETS_TO_CACHE = [
   '/',
     '/index.html',
     '/css/site.css',
-    '/js/config.js?v=1777929942',
-    '/js/app.js?v=1777929942',
-    '/js/modulos/cadastros.js?v=1777929942',
-    '/js/modulos/financeiro.js?v=1777929942',
-    '/js/modulos/pedagogico.js?v=1777929942',
-    '/js/modulos/relatorios.js?v=1777929942'
+    '/js/config.js?v=1777931853',
+    '/js/app.js?v=1777931853',
+    '/js/modulos/cadastros.js?v=1777931853',
+    '/js/modulos/financeiro.js?v=1777931853',
+    '/js/modulos/pedagogico.js?v=1777931853',
+    '/js/modulos/relatorios.js?v=1777931853'
 ];
 
 // 2. INSTALAÇÃO: Força a ativação imediata e guarda os ficheiros estáticos
