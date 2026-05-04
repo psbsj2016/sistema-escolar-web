73289e9a0e654deafc7c23fe03e353a79ef6aa3f
psbsj2016 <psbsj.2016@gmail.com>
2026-05-04 20:32:16 -0300

    🚀 Agora sim, enviando o Fura-CDN verdadeiro

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // 1. GESTÃO DE VERSÃO DINÂMICA
 const APP_PREFIX = 'escola-pwa-';
 // 🚀 SEMPRE QUE ATUALIZAR O SISTEMA FRONT-END, MUDE ESTE NÚMERO
-const VERSION = 'v1777936855'; 
+const VERSION = 'v1777937530'; 
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 // Ficheiros essenciais para o ecrã inicial funcionar super rápido
@@ -13,12 +13,12 @@ const ASSETS_TO_CACHE = [
   '/',
     '/index.html',
     '/css/site.css',
-    '/js/config.js?v=1777936855',
-    '/js/app.js?v=1777936855',
-    '/js/modulos/cadastros.js?v=1777936855',
-    '/js/modulos/financeiro.js?v=1777936855',
-    '/js/modulos/pedagogico.js?v=1777936855',
-    '/js/modulos/relatorios.js?v=1777936855'
+    '/js/config.js?v=1777937530',
+    '/js/app.js?v=1777937530',
+    '/js/modulos/cadastros.js?v=1777937530',
+    '/js/modulos/financeiro.js?v=1777937530',
+    '/js/modulos/pedagogico.js?v=1777937530',
+    '/js/modulos/relatorios.js?v=1777937530'
 ];
 
 // 2. INSTALAÇÃO: Força a ativação imediata e guarda os ficheiros estáticos
