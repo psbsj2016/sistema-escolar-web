95718f3cea60d8f8bea972f442e63a85af79f2d1
psbsj2016 <psbsj.2016@gmail.com>
2026-05-05 20:43:33 -0300

    Correcao final dos dados no cofre

--- sw.js
+++ sw.js
@@ -3,20 +3,20 @@
 // =========================================================
 
 const APP_PREFIX = 'escola-pwa-';
-const VERSION = 'v1778020799'; // O seu robô vai mudar isto automaticamente
+const VERSION = 'v1778024606'; // O seu robô vai mudar isto automaticamente
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 // ⚠️ As tags ?v= estão aqui para o seu robô as poder atualizar
 const ASSETS_TO_CACHE = [
     '/',
     '/index.html',
-    '/css/site.css?v=1778020799', 
-    '/js/config.js?v=1778020799',
-    '/js/app.js?v=1778020799',
-    '/js/modulos/cadastros.js?v=1778020799',
-    '/js/modulos/financeiro.js?v=1778020799',
-    '/js/modulos/pedagogico.js?v=1778020799',
-    '/js/modulos/relatorios.js?v=1778020799'
+    '/css/site.css?v=1778024606', 
+    '/js/config.js?v=1778024606',
+    '/js/app.js?v=1778024606',
+    '/js/modulos/cadastros.js?v=1778024606',
+    '/js/modulos/financeiro.js?v=1778024606',
+    '/js/modulos/pedagogico.js?v=1778024606',
+    '/js/modulos/relatorios.js?v=1778024606'
 ];
 
 self.addEventListener('install', (event) => {
