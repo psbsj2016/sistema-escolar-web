ced625609f82465ad5ca33b327f9fd717e19eec5
psbsj2016 <psbsj.2016@gmail.com>
2026-05-04 22:19:08 -0300

    🚀 Correção: Adicionados todos os campos ao payload de matrícula

--- sw.js
+++ sw.js
@@ -3,20 +3,20 @@
 // =========================================================
 
 const APP_PREFIX = 'escola-pwa-';
-const VERSION = 'v1777943554'; // O seu robô vai mudar isto automaticamente
+const VERSION = 'v1777943943'; // O seu robô vai mudar isto automaticamente
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 // ⚠️ As tags ?v= estão aqui para o seu robô as poder atualizar
 const ASSETS_TO_CACHE = [
     '/',
     '/index.html',
-    '/css/site.css?v=1777943554', 
-    '/js/config.js?v=1777943554',
-    '/js/app.js?v=1777943554',
-    '/js/modulos/cadastros.js?v=1777943554',
-    '/js/modulos/financeiro.js?v=1777943554',
-    '/js/modulos/pedagogico.js?v=1777943554',
-    '/js/modulos/relatorios.js?v=1777943554'
+    '/css/site.css?v=1777943943', 
+    '/js/config.js?v=1777943943',
+    '/js/app.js?v=1777943943',
+    '/js/modulos/cadastros.js?v=1777943943',
+    '/js/modulos/financeiro.js?v=1777943943',
+    '/js/modulos/pedagogico.js?v=1777943943',
+    '/js/modulos/relatorios.js?v=1777943943'
 ];
 
 self.addEventListener('install', (event) => {
