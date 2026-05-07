9d284d625ec8088988530c312671a7048893ce27
psbsj2016 <psbsj.2016@gmail.com>
2026-05-07 00:18:53 -0300

    Correção de barras duplas na URL

--- sw.js
+++ sw.js
@@ -3,20 +3,20 @@
 // =========================================================
 
 const APP_PREFIX = 'escola-pwa-';
-const VERSION = 'v1778123629'; // O seu robô vai mudar isto automaticamente
+const VERSION = 'v1778123929'; // O seu robô vai mudar isto automaticamente
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 // ⚠️ As tags ?v= estão aqui para o seu robô as poder atualizar
 const ASSETS_TO_CACHE = [
     '/',
     '/index.html',
-    '/css/site.css?v=1778123629', 
-    '/js/config.js?v=1778123629',
-    '/js/app.js?v=1778123629',
-    '/js/modulos/cadastros.js?v=1778123629',
-    '/js/modulos/financeiro.js?v=1778123629',
-    '/js/modulos/pedagogico.js?v=1778123629',
-    '/js/modulos/relatorios.js?v=1778123629'
+    '/css/site.css?v=1778123929', 
+    '/js/config.js?v=1778123929',
+    '/js/app.js?v=1778123929',
+    '/js/modulos/cadastros.js?v=1778123929',
+    '/js/modulos/financeiro.js?v=1778123929',
+    '/js/modulos/pedagogico.js?v=1778123929',
+    '/js/modulos/relatorios.js?v=1778123929'
 ];
 
 self.addEventListener('install', (event) => {
