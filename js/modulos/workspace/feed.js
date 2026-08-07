9d395f565c75a9c5146bbacf7665a5efa8b10099
psbsj2016 <psbsj.2016@gmail.com>
2026-08-07 20:41:00 -0300

    feat(workspace): implementa controle de inatividade de sessao e anexa indicador online globalmente aos avatares

--- js/modulos/workspace/feed.js
+++ js/modulos/workspace/feed.js
@@ -419,7 +419,7 @@ Workspace.Feed = {
         sentinela.style.display = 'block';
         
         // 🚀 A CURA VISUAL: Removemos a instrução vermelha do programador e deixamos um aviso profissional
-        sentinela.innerHTML = '<div style="text-align:center; padding:20px; color:#999; font-size:13px;"><strong>A carregar publicações... ⏳</strong></div>';
+        sentinela.innerHTML = '<div style="text-align:center; padding:20px; color:#999; font-size:13px;"><strong>Atualizando feed!... ⏳ Por favor, saia e entre novamente.</strong></div>';
 
         Workspace.Feed.carregarLoteFiltrado(listaFiltrada);
     },
